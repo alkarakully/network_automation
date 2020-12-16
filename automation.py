f1a69a057d5e0daf52d3560a420229ab7bab7387
alkarakully <alkarakully@gmail.com>
2020-12-16 16:54:55 +0000

    Fix typo in add ssh key

--- automation.py
+++ automation.py
@@ -10,7 +10,7 @@ hostname = input("Hostname: ")
 
 
 session = paramiko.SSHClient()
-session.set_missing_host_ket_policy(paramiko.AutoAddPolicy())
+session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 session.connect(hostname, port=22, username= username, password=password)
 
 stdin, stdout, stderr = session.exec_command("show ip interface brief")
